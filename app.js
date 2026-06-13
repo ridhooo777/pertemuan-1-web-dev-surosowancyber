f55caa0d0797aacc162ff3821852a96abddc8896
ridhooo777 <ridodudin07@gmail.com>
2026-06-13 17:07:11 +0700

    update app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// Operator Aritmatika
+let a = 10;
+let b = 5;
+
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0
+console.log(a ** b); // 100000
+
+
+
+// Operator Perbandingan
+let nilai = 80;
+
+console.log(nilai > 70); // true
+console.log(nilai < 70); // false
+console.log(nilai == "80"); // true
+console.log(nilai !== 80); // true
+
+// Operator Logika
+let usernameBenar = true;
+let passwordBenar = true;
+
+console.log(usernameBenar && passwordBenar); // true
+console.log(usernameBenar || passwordBenar); // true
+console.log(!usernameBenar);
